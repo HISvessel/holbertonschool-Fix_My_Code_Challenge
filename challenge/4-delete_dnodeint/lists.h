90cd8d0890b5ef5e3de463287927090586cf2202
HISvessel <kevinsanchezdabest@yahoo.com>
2025-09-22 17:57:48 -0400

    correcting function name in the header file. function is now referenced correctly

--- challenge/4-delete_dnodeint/lists.h
+++ challenge/4-delete_dnodeint/lists.h
@@ -21,7 +21,7 @@ typedef struct dlistint_s
 } dlistint_t;
 
 size_t print_dlistint(const dlistint_t *h);
-dlistint_t *add_dondeint_end(dlistint_t **head, const int n);
+dlistint_t *add_dnodeint_end(dlistint_t **head, const int n);
 int delete_dnodeint_at_index(dlistint_t **head, unsigned int index);
 void free_dlistint(dlistint_t *head);
 
