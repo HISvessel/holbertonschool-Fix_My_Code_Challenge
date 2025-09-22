fa7d35c2f158906386925b348ad7d9067c1dc109
HISvessel <kevinsanchezdabest@yahoo.com>
2025-09-22 17:55:27 -0400

    corrected file errors in the header files. adding the correct typedef'd structure names for correct reference calls

--- challenge/4-delete_dnodeint/lists.h
+++ challenge/4-delete_dnodeint/lists.h
@@ -20,8 +20,8 @@ typedef struct dlistint_s
 	struct dlistint_s *next;
 } dlistint_t;
 
-size_t print_dlistint(const dlistint *h);
-dlistint_t *add_dondeint_end(dlistint **head, const int n);
+size_t print_dlistint(const dlistint_t *h);
+dlistint_t *add_dondeint_end(dlistint_t **head, const int n);
 int delete_dnodeint_at_index(dlistint_t **head, unsigned int index);
 void free_dlistint(dlistint_t *head);
 
